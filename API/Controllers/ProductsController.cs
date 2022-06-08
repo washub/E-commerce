017da4979e13350d5bb7fe9ca67b54e7a4321fd2
Wasique <wumar42@gmail.com>
2022-06-08 08:28:50 +0530

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            var prod =  await _context.Products.ToListAsync();
+            return Ok(prod);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id){
+            var prod = await _context.Products.Where(p=> p.Id==id).FirstOrDefaultAsync();
+            if(prod == null){
+                return NotFound(new Product{Name="No data found"});
+            }
+            return Ok(prod);
+        }
+    }
+}
