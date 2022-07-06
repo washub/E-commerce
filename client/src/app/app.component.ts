26d08328e1041cc9cd92d9359f1618dfeb174768
Wasique <wumar42@gmail.com>
2022-07-06 13:41:10 +0530

    Added Navbar_Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './Models/pagination';
+import { IProduct } from './Models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'E-Commerce';
+  products:IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void{
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response:IPagination) =>{
+      this.products = response.data;
+    },
+    err =>{
+      console.log(err);
+        
+    });
+
+  }
 }
